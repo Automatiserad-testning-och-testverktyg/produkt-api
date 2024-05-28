e14eb974596c8859d3e7637c24916fe7d2d293d8
linnbergstrom <linn.bergstrom@iths.se>
2024-05-28 19:20:05 +0200

    Product controller test added

---
+++ src/test/java/com/example/produktapi/controller/ProductControllerTest.java
@@ -0,0 +1,29 @@
+package com.example.produktapi.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @Test
+    void hello() {
+
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void getAllCategories() {
+    }
+
+    @Test
+    void getProductsByCategory() {
+    }
+
+    @Test
+    void getProductById() {
+    }
+}
