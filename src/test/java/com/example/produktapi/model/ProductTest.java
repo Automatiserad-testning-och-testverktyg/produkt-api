0b3971d7771af4c71a9fb30e2ff0c5096ba688ac
swapHar <119521871+swapHar@users.noreply.github.com>
2024-05-28 19:32:50 +0200

    Product test added

---
+++ src/test/java/com/example/produktapi/model/ProductTest.java
@@ -0,0 +1,56 @@
+package com.example.produktapi.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void setTitle() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void setPrice() {
+    }
+
+    @Test
+    void getCategory() {
+    }
+
+    @Test
+    void setCategory() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void getImage() {
+    }
+
+    @Test
+    void setImage() {
+    }
+}
