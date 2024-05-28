268a88d8af82f5b3354af855a206b7f7a96c5f15
CardonaCervantes <sebastian.cardonacervantes@firefly.se>
2024-05-28 23:29:15 +0200

    Added ProductRepositoryTest

---
+++ src/test/java/com/example/produktapi/repository/ProductRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.example.produktapi.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepositoryTest {
+
+    @Test
+    void findByCategory() {
+    }
+
+    @Test
+    void findByTitle() {
+    }
+
+    @Test
+    void findAllCategories() {
+    }
+}
