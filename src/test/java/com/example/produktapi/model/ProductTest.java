093e121a2b3c6f7489942bc349f5f7984058f05b
bagch <swetabagchi2014@gmail.com>
2024-06-01 18:07:42 +0200

    Adding test cases for ProductTest.java

--- src/test/java/com/example/produktapi/model/ProductTest.java
+++ src/test/java/com/example/produktapi/model/ProductTest.java
@@ -1,56 +1,56 @@
 package com.example.produktapi.model;
 
+import com.example.produktapi.service.ProductService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
 
-    @Test
-    void getId() {
-    }
+    //Testcases by Sweta
+
+
+    private Product product = new Product("Test Product", 10.0, "Test Category", "Test Description", "https://example.com/img/placeholder.jpg");
+    private Product product2 = new Product("Test Product 2", 20.0, "Test Category 2", "Test Description 2", "https://example.com/img/placeholder2.jpg");
 
     @Test
-    void setId() {
+    void getId_testingTheIdOfTheNewProduct() {
+        // Assertions.assertEquals();
+
     }
+
 
     @Test
     void getTitle() {
-    }
+        Assertions.assertAll(
+                () ->  assertEquals("Test Product", product.getTitle()),
+                () ->  assertNotEquals("Test Product 23", product2.getTitle())
+        );
 
-    @Test
-    void setTitle() {
     }
 
     @Test
     void getPrice() {
     }
 
-    @Test
-    void setPrice() {
-    }
 
     @Test
     void getCategory() {
     }
 
-    @Test
-    void setCategory() {
-    }
 
     @Test
     void getDescription() {
     }
 
     @Test
-    void setDescription() {
-    }
-
-    @Test
     void getImage() {
-    }
-
-    @Test
-    void setImage() {
     }
 }
