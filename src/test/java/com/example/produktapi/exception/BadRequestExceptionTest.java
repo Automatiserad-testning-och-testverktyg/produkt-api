fd36107b4799b05fa6bc6111d61d39b2921b6eb6
CardonaCervantes <144700510+CardonaCervantes@users.noreply.github.com>
2024-06-26 19:18:17 +0200

    Update BadRequestExceptionTest.java
    added name

--- src/test/java/com/example/produktapi/exception/BadRequestExceptionTest.java
+++ src/test/java/com/example/produktapi/exception/BadRequestExceptionTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BadRequestExceptionTest {
+class BadRequestExceptionTest { // Skrivet av Jamie Blomerus
 
     @Test
     @DisplayName("Exception message is set correctly")
